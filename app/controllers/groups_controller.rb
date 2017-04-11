0f8287117c1e42eb83f234dd30f169e06973ed2e
liangning001 <liangning001@gmail.com>
2017-04-11 12:00:31 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,8 +5,9 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
   def show
-    @group = Group.find(params[:id])
-  end
+   @group = Group.find(params[:id])
+   @posts = @group.posts
+ end
   def edit
 
   end
@@ -60,5 +61,3 @@ end
    def group_params
      params.require(:group).permit(:title, :description)
    end
-
- 
