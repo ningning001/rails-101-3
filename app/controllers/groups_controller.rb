b4270b1aae5bf7549855f4eba41fa750dffe3357
liangning001 <liangning001@gmail.com>
2017-04-11 17:22:05 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   def index
     @groups = Group.all
   end
@@ -10,28 +10,24 @@ class GroupsController < ApplicationController
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
-
   def edit
-
+    @group = Group.find(params[:id])
   end
-end
+
   def new
   @group = Group.new
- end
-
-
- def create
+  end
+  def create
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
     end
   end
   def update
-
-
 
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
@@ -42,25 +38,46 @@ end
 
   def destroy
 
-
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
 
-    @group.destroy
-    redirect_to groups_path, alert: "Group deleted"
+  end
+  def join
+  @group = Group.find(params[:id])
+
+   if !current_user.is_member_of?(@group)
+     current_user.join!(@group)
+     flash[:notice] = "加入本讨论版成功！"
+   else
+     flash[:warning] = "你已经是本讨论版成员了！"
+   end
+
+   redirect_to group_path(@group)
+ end
+
+ def quit
+   @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+   redirect_to group_path(@group)
   end
 
  private
-
 
  def find_group_and_check_permission
      @group = Group.find(params[:id])
 
      if current_user != @group.user
        redirect_to root_path, alert: "You have no permission."
-     end
-   end
 
-   def group_params
-     params.require(:group).permit(:title, :description)
+def group_params
+ params.require(:group).permit(:title, :description)
+
+     end
    end
